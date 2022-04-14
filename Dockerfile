761290cf801fd99b0945f96a0f3c89dbc2f77b15
zeze.li <franklzz@foxmail.com>
2022-04-14 17:49:17 +0800

    安装nvm

--- Dockerfile
+++ Dockerfile
@@ -12,13 +12,13 @@ RUN /bin/sh -c yes | sh /root/on-my-zsh.sh
 RUN /bin/sh -c  zsh
 ADD exa-linux-x86_64-v0.10.1/* /root/.exa/
 ADD bashrc /root/.bashrc
-# ENV NVM_DIR /root/.nvm
-# ENV NODE_VERSION v11.0.0
 
-# # Install nvm with node and npm
-# ADD nvm.sh /root/
-# RUN chmod 777 /root/nvm.sh
-# RUN  /bin/sh -c sh /root/nvm.sh 
-# RUN  nvm install $NODE_VERSION
-# RUN npm config set registry https://registry.npm.taobao.org
-# RUN npm i -g yarn
+
+ENV NVM_DIR /root/.nvm
+ENV NODE_VERSION 10.15.3
+ADD nvm.sh /root/
+RUN chmod 777 /root/nvm.sh
+# Install nvm with node and npm
+RUN bash -c   "/root/nvm.sh" \
+  && . $NVM_DIR/nvm.sh \ 
+  && nvm install $NODE_VERSION
--- nvm.sh
+++ nvm.sh
@@ -1,13 +1,4 @@
-export NVM_DIR="$HOME/.nvm" && (
-  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-  cd "$NVM_DIR"
-  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-) && \. "$NVM_DIR/nvm.sh"
-
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This lo#!/usr/bin/env bash
+ #!/usr/bin/env bash
 
 { # this ensures the entire script is downloaded #
 
@@ -431,4 +422,4 @@ nvm_reset() {
 
 [ "_$NVM_ENV" = "_testing" ] || nvm_do_install
 
-} # this ensures the entire script is downloaded # ads nvm bash_completion
+} # this ensures the entire script is downloaded #
